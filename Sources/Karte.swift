da7340656d8317dcf533f934a453b88d9c484d75
Kilian Koeltzsch <me@kilian.io>
2018-05-14 11:53:56 +0200

    add documentation to public api

--- Sources/Karte.swift
+++ Sources/Karte.swift
@@ -11,12 +11,26 @@ import struct CoreLocation.CLLocationCoordinate2D
 import MapKit
 
 public enum Karte {
+    /// Check if a navigation app is installed on the device.
+    ///
+    /// - Parameter app: a navigation app supported by Karte
+    /// - Returns: `true` if the app is installed
+    /// - Warning: For this to return `true` in any case, the necessary url schemes have to be included in your app's Info.plist.
+    /// Please see Karte's README for additional details.
     public static func isInstalled(_ app: MapsApp) -> Bool {
         guard app != .appleMaps else { return true }
         guard let url = URL(string: app.urlScheme) else { return false }
         return UIApplication.shared.canOpenURL(url)
     }
 
+    /// Try to launch a navigation app with the given parameters
+    ///
+    /// - Parameters:
+    ///   - app: the app to be launched
+    ///   - origin: an optional origin location, defaults to the user's location if left empty in most apps
+    ///   - destination: the location to route to
+    ///   - mode: an optional mode of transport to use
+    /// - Throws: `Karte.Error.unsupportedMode` if the chosen mode is not supported by the target app
     public static func launch(app: MapsApp, origin: LocationRepresentable? = nil, destination: LocationRepresentable, mode: Mode? = nil) throws {
         guard self.isInstalled(app) else { throw Error.notInstalled }
 
@@ -33,12 +47,25 @@ public enum Karte {
         }
 
         guard let url = URL(string: try app.queryString(origin: origin, destination: destination, mode: mode)) else {
-            throw Error.malformedURL // There's not really a lot the user could do about this, is there?
+            // There's not really a lot the user could do about this, is there?
+            // Maybe fail hard here and validate through tests?
+            throw Error.malformedURL
         }
 
         UIApplication.shared.open(url, completionHandler: nil)
     }
 
+    /// Present a `UIAlertController` with all supported apps the device has installed to offer an option for which app to start.
+    ///
+    /// - Parameters:
+    ///   - origin: an optional origin location, defaults to the user's location if left empty in most apps
+    ///   - destination: the location to route to
+    ///   - mode: an optional mode of transport to use, results in only those apps being shown that support this mode
+    ///   - viewcontroller: a `UIViewController` to present the `UIAlertController` on
+    ///   - title: an optional title for the `UIAlertController`
+    ///   - message: an optional message for the `UIAlertController`
+    ///   - cancel: label for the cancel button, defaults to "Cancel"
+    ///   - style: the `UIAlertController`'s style, defaults to `.actionSheet`
     public static func presentPicker(origin: LocationRepresentable? = nil, destination: LocationRepresentable, mode: Mode? = nil, presentOn viewcontroller: UIViewController, title: String? = nil, message: String? = nil, cancel: String = "Cancel", style: UIAlertControllerStyle = .actionSheet) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: style)
         MapsApp.all
