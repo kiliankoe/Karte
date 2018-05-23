9fafa6a769de2b3c3c08b0d1d7dd109439496f2a
Kilian Koeltzsch <me@kilian.io>
2018-05-24 00:03:40 +0200

    slight test cleanup

--- Tests/KarteTests/KarteTests.swift
+++ Tests/KarteTests/KarteTests.swift
@@ -10,10 +10,7 @@ import Foundation
 import XCTest
 @testable import Karte
 
-// swiftlint:disable force_try
-
 import struct CoreLocation.CLLocationCoordinate2D
-let dummyCoordinate = CLLocationCoordinate2D(latitude: 10.0, longitude: 10.0)
 
 class KarteTests: XCTestCase {
     func testExistenceOfMapsApps() {
@@ -25,19 +22,20 @@ class KarteTests: XCTestCase {
     }
 
     func testQueryStrings() {
+        let dummyCoordinate = CLLocationCoordinate2D(latitude: 10.0, longitude: 10.0)
         let anonLoc = Location(coordinate: dummyCoordinate)
         let namedLoc = Location(name: "Named Location", coordinate: dummyCoordinate)
         let fullLoc = Location(name: "Full Location", address: "Location Address", coordinate: dummyCoordinate)
 
-        XCTAssertEqual(try! App.googleMaps.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "comgooglemaps://maps?daddr=10.0,10.0&saddr=10.0,10.0")
-        XCTAssertEqual(try! App.citymapper.queryString(origin: namedLoc, destination: fullLoc, mode: nil), "citymapper://directions?endaddress=Location%20Address&endcoord=10.0,10.0&endname=Full%20Location&startcoord=10.0,10.0&startname=Named%20Location")
-        XCTAssertEqual(try! App.transit.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "transit://directions?from=10.0,10.0&to=10.0,10.0")
-        XCTAssertEqual(try! App.lyft.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "lyft://ridetype?id=lyft&destination[latitude]=10.0&destination[longitude]=10.0&pickup[latitude]=10.0&pickup[longitude]=10.0")
-        XCTAssertEqual(try! App.uber.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "uber://?action=setPickup&dropoff[latitude]=10.0&dropoff[longitude]=10.0&dropoff[nickname]=Named%20Location&pickup[latitude]=10.0&pickup[longitude]=10.0")
-        XCTAssertEqual(try! App.navigon.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "navigon://coordinate/Named%20Location/10.0/10.0")
-        XCTAssertEqual(try! App.waze.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "waze://?ll=10.0,10.0&navigate=yes")
-        XCTAssertEqual(try! App.yandex.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "yandexnavi://build_route_on_map?lat_from=10.0&lat_to=10.0&lon_from=10.0&lon_to=10.0")
-        XCTAssertEqual(try! App.moovit.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "moovit://directions?dest_lat=10.0&dest_lon=10.0&dest_name=Named%20Location&origin_lat=10.0&origin_lon=10.0")
+        XCTAssertEqual(App.googleMaps.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "comgooglemaps://maps?daddr=10.0,10.0&saddr=10.0,10.0")
+        XCTAssertEqual(App.citymapper.queryString(origin: namedLoc, destination: fullLoc, mode: nil), "citymapper://directions?endaddress=Location%20Address&endcoord=10.0,10.0&endname=Full%20Location&startcoord=10.0,10.0&startname=Named%20Location")
+        XCTAssertEqual(App.transit.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "transit://directions?from=10.0,10.0&to=10.0,10.0")
+        XCTAssertEqual(App.lyft.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "lyft://ridetype?id=lyft&destination[latitude]=10.0&destination[longitude]=10.0&pickup[latitude]=10.0&pickup[longitude]=10.0")
+        XCTAssertEqual(App.uber.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "uber://?action=setPickup&dropoff[latitude]=10.0&dropoff[longitude]=10.0&dropoff[nickname]=Named%20Location&pickup[latitude]=10.0&pickup[longitude]=10.0")
+        XCTAssertEqual(App.navigon.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "navigon://coordinate/Named%20Location/10.0/10.0")
+        XCTAssertEqual(App.waze.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "waze://?ll=10.0,10.0&navigate=yes")
+        XCTAssertEqual(App.yandex.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "yandexnavi://build_route_on_map?lat_from=10.0&lat_to=10.0&lon_from=10.0&lon_to=10.0")
+        XCTAssertEqual(App.moovit.queryString(origin: anonLoc, destination: namedLoc, mode: nil), "moovit://directions?dest_lat=10.0&dest_lon=10.0&dest_name=Named%20Location&origin_lat=10.0&origin_lon=10.0")
     }
 
     func testIsInstalled() {
