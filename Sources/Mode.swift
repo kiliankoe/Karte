83798fae424a3ea17ebbcb7c696def500deaf2b1
Kilian Koeltzsch <me@kilian.io>
2018-05-23 22:05:41 +0200

    split up mode functions
    really disliking that `Any?` there... Let's see how that
    works out

--- Sources/Mode.swift
+++ Sources/Mode.swift
@@ -16,25 +16,20 @@ public enum Mode {
     case transit
     case taxi
 
-    func appleMaps() throws -> [String: String] {
-        switch self {
-        case .walking: return [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking]
-        case .driving: return [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
-        case .transit: return [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeTransit]
-        case .taxi: return [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving] // it is supported, but there's no key for this...
-        default: throw Error.unsupportedMode
-        }
-    }
-
-    /// Look up transport mode parameter names for a given app.
+    /// Look up the transport mode parameter names for a given app.
     ///
     /// - Parameter app: navigation app
-    /// - Returns: empty string if parameter is irrelevant, but supported
-    /// - Throws: `unsupportedMode` if app doesn't support this mode
-    func supported(by app: MapsApp) throws -> String { // swiftlint:disable:this cyclomatic_complexity
+    /// - Returns: For Apple Maps a `[String:String]`, `String` for anything else. `nil` if irrelevant.
+    func parameter(for app: MapsApp) -> Any? { // swiftlint:disable:this cyclomatic_complexity
         switch app {
         case .appleMaps:
-            return "" // yay for special ❄️'s
+            switch self {
+            case .walking: return [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking]
+            case .driving: return [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+            case .transit: return [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeTransit]
+            case .taxi: return [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving] // it is supported, but there's no key for this...
+            default: return nil
+            }
         case .googleMaps:
             switch self {
             case .walking: return "walking"
@@ -44,30 +39,38 @@ public enum Mode {
             case .taxi: return "driving" // just like Apple Maps this actually is supported, but the key isn't specified in the docs... meh.
             }
         case .citymapper, .transit:
-            if self != .transit {
-                throw Error.unsupportedMode
-            }
+            return nil
         case .lyft, .uber:
-            if self != .taxi {
-                throw Error.unsupportedMode
-            }
+            return nil
         case .navigon:
-            switch self {
-            case .driving, .walking:
-                return ""
-            default:
-                throw Error.unsupportedMode
-            }
+            return nil
         case .waze:
-            if self != .driving {
-                throw Error.unsupportedMode
-            }
+            return nil
         case .yandex:
-            return ""
+            return nil
         case .moovit:
-            return ""
+            return nil
         }
+    }
 
-        return ""
+    func supported(by app: MapsApp) -> Bool {
+        switch app {
+        case .appleMaps:
+            return self != .bicycling
+        case .googleMaps:
+            return true
+        case .citymapper, .transit:
+            return self == .transit
+        case .lyft, .uber:
+            return self == .taxi
+        case .navigon:
+            return self == .driving || self == .walking
+        case .waze:
+            return self == .driving
+        case .yandex:
+            return true
+        case .moovit:
+            return true
+        }
     }
 }
